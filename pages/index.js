26096b5a5dbd2357a8bdc084253846836e1114b5
German Cutraro <germancutraro@hotmail.com>
2020-05-26 18:58:31 -0300

    global styles

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Content from '../components/Content';
 // Styles
 import NavBar from '../components/Navbar';
+import { GlobalStyles } from '../styles/globalStyles';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <title>Germán Cutraro - Full Stack JavaScript Developer</title>
         <link rel='shortcut icon' href='/favicon.png' />
       </Head>
+      <GlobalStyles />
       <NavBar />
       <Content />
     </>
---
+++ styles/globalStyles.js
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+export const GlobalStyles = createGlobalStyle`
+${normalize}
+
+ body {
+   --bgColor: #FAF5F2;
+   --textColor: #22211D;
+   --iconColor: #000;
+ }
+
+ *,
+ *::after,
+ *::before {
+   box-sizing: border-box;
+   margin: 0;
+ }
+
+ html, body {
+   background-color: var(--bgColor);
+   -webkit-touch-callout: none; 
+   -webkit-user-select: none; 
+   -khtml-user-select: none;
+   -moz-user-select: none; 
+   -ms-user-select: none; 
+   user-select: none; 
+ }
+
+ #__next {
+   display: flex;
+   flex-direction: column;
+   min-height: 100vh;
+   width: 100vw;
+ }
+
+ body {
+   background-color: var(--bgColor);
+   transition: background-color 0.2s linear;
+ }
+
+ body.dark {
+   --bgColor: #1B1C22;
+   --textColor: white;
+   --iconColor: #fff;
+ }
+`;
