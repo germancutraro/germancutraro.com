fcc4a0ba72d9e6835f18fa06782eef972ef4a5fe
germancutraro <cutraroger@gmail.com>
2024-07-19 13:31:15 -0300

    Change title

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
-import Head from 'next/head';
-import Content from '../components/Content';
+import Head from "next/head";
+import Content from "../components/Content";
 // Styles
-import NavBar from '../components/Navbar';
-import { GlobalStyles } from '../styles/globalStyles';
+import NavBar from "../components/Navbar";
+import { GlobalStyles } from "../styles/globalStyles";
 
 function App() {
   return (
     <>
       <Head>
-        <title>Germán Cutraro - Backend Engineer</title>
+        <title>Germán Cutraro - Full Stack Engineer</title>
         <meta
-          name='description'
-          content='Software developer specialized in JavaScript, working on multiple projects both front-end and back-end (Node.js, Express.js, PostgreSQL, Typescript, AWS, etc).
+          name="description"
+          content="Software developer specialized in JavaScript, working on multiple projects both front-end and back-end (Node.js, Express.js, PostgreSQL, Typescript, AWS, etc).
 I focus on creating scalable products, following good practices to achieve better performance. I also work / worked as CTO, Full Stack Developer, Mobile Developer, Software Consultant in multiple projects abroad.
 
 Passionate about the cloud, architectures and everything related to Amazon Web Services!
-I love learning, training people and working on innovative projects.'
+I love learning, training people and working on innovative projects."
         />
-        <link rel='shortcut icon' href='/favicon.png' />
+        <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <GlobalStyles />
       <NavBar />
